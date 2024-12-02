62e219e5ffe92a7bf54dd2c56d6cceecd16791e3
abigailmarquez <abigail2002.am@gmail.com>
2024-12-01 20:46:24 -0600

    Agregar google-drive-service-account.json a .gitignore 3

--- tasks/google_drive_utils.py
+++ tasks/google_drive_utils.py
@@ -2,9 +2,18 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.oauth2.service_account import Credentials
 import os
+import json
 
-# Carga las credenciales desde settings
+# Cargar las credenciales desde la variable de entorno de Render
 from django.conf import settings
+
+def get_credentials_from_env():
+    """
+    Obtiene las credenciales desde la variable de entorno.
+    """
+    credentials_info = json.loads(settings.GOOGLE_DRIVE_CREDENTIALS)
+    credentials = Credentials.from_service_account_info(credentials_info)
+    return credentials
 
 def upload_to_drive(file_path, file_name, folder_id=None):
     """
@@ -14,7 +23,7 @@ def upload_to_drive(file_path, file_name, folder_id=None):
     :param folder_id: ID de la carpeta en Google Drive (opcional).
     :return: ID del archivo subido.
     """
-    credentials = Credentials.from_service_account_file(settings.GOOGLE_DRIVE_CREDENTIALS)
+    credentials = get_credentials_from_env()
     drive_service = build('drive', 'v3', credentials=credentials)
 
     file_metadata = {'name': file_name}
@@ -37,7 +46,7 @@ def set_file_public(file_id):
     Hace que un archivo sea público.
     :param file_id: ID del archivo en Google Drive.
     """
-    credentials = Credentials.from_service_account_file(settings.GOOGLE_DRIVE_CREDENTIALS)
+    credentials = get_credentials_from_env()
     drive_service = build('drive', 'v3', credentials=credentials)
 
     permission = {
